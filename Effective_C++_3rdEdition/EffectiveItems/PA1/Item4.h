dd3ef4c760ae07709ec9f2586e61a5dea93c24c9
ljsman@msn.com <ljsman@msn.com>
2019-12-31 17:15:50 -0600

    update boost lib

--- Effective_C++_3rdEdition/EffectiveItems/PA1/Item4.h
+++ Effective_C++_3rdEdition/EffectiveItems/PA1/Item4.h
@@ -12,15 +12,35 @@ public:
 			return singleton;
 		}
 		//public member functions
+		static int* getData()
+		{
+			return Singleton::getInstance().privGetData();
+		}
+		
+	private:
+		int data;
+
+		int* privGetData()
+		{
+			return &(this->data); 
+		}
+		explicit constexpr  Singleton() noexcept
+			: data{}
+		{
+			
+		}
+
+		explicit constexpr  Singleton(const int t) noexcept
+			: data{t}
+		{
+
+		}
+		
 		~Singleton() = default;
 		Singleton(const Singleton&) = delete;
 		Singleton& operator = (const Singleton&) = delete;
 		Singleton(Singleton&&) = delete;
 		Singleton& operator = (Singleton&&) = delete;
-		
-	private:
-		Singleton() = default;
-		
 		
 	};
 };
--- Effective_C++_3rdEdition/EffectiveItems/PA1/ItemHeader.h
+++ Effective_C++_3rdEdition/EffectiveItems/PA1/ItemHeader.h
@@ -1,5 +1,6 @@
 #pragma once
 using namespace std;
+#include "Item4.h"
 #include "Item7.h"
 #include "TestingClass.h"
 #include "A.h"
