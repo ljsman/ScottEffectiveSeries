1a55777c0aae1c9919f4ed8986a440784a2484ad
ljsman@msn.com <ljsman@msn.com>
2020-02-10 18:15:07 -0600

    heapify max

---
+++ SelfAdded/EffectiveItems/PA1/Heap.cpp
@@ -0,0 +1,16 @@
+#include "Heap.h"
+Heap::Heap()
+	: data()
+{
+	Trace::out("Heap Object Constructed\n");
+}
+
+void Heap::build_max_heap()
+{
+
+}
+
+void Heap::max_heapify()
+{
+	Trace::out("Heapifing Max");
+}
